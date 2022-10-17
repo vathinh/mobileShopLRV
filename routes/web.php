1512622ed13b7d5784dc09fa0608e5c74b13138e
vathinh <tv.thinh2000@gmail.com>
2022-10-17 20:58:54 +0700

    ad

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Auth::routes();
 // Route User
 Route::middleware(['auth','user-role:user'])->group(function()
 {
-    
+
     Route::get("/home",[HomeController::class, 'userHome'])->name("userDB.userDB");
 });
 // Route Editor
@@ -32,6 +32,7 @@ Route::middleware(['auth','user-role:editor'])->group(function()
 {
     Route::get("/editor/home",[HomeController::class, 'editorHome'])->name("editor.home");
 });
+
 // Route Admin
 Route::middleware(['auth','user-role:admin'])->group(function()
 {
