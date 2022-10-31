eb1b33c8b5349b18716d09874db14cf20d7b2acb
phuocthaoins <phuocthao.ins@gmail.com>
2022-10-31 15:27:45 +0700

    Update productDetails

--- resources/views/newWelcome.blade.php
+++ resources/views/newWelcome.blade.php
@@ -46,7 +46,7 @@
                 </a>
             </div>
         </aside>
-            
+
 
         <!-- PAGE CONTAINER-->
         <div class="page-container">
@@ -62,128 +62,124 @@
                                 </button>
                             </form>
                             <div class="account-wrap">
-                                    <div class="account-item clearfix js-item-menu">
-                                        
-                                            
-                                       
-                                        <div class="content">
-                                            <a class="js-acc-btn" href="#">
+                                <div class="account-item clearfix js-item-menu">
+                                    <div class="content">
+                                        <a class="js-acc-btn" href="#">
                                             <i class="fa fa-shopping-cart">Cart</i> <span class="badge badge-pill badge-danger">{{ count((array) session('cart')) }}</span>
-                                            </a>
-                                        </div>
-                                        <div class="account-dropdown js-dropdown">
-                                            <!-- user profile img -->
-                                            <div class="info clearfix">
-                                               
-                                                <a href="#">
-                                                        <i class="fa fa-shopping-cart"> Cart</i> <span class="badge badge-pill badge-danger">{{ count((array) session('cart')) }}</span>
-                                                </a>
-                                               
-                                          
-                                            </div>
+                                        </a>
+                                    </div>
+                                    <div class="account-dropdown js-dropdown">
+                                        <!-- user profile img -->
+                                        <div class="info clearfix">
 
-                                            <div class="account-dropdown__body">
+                                            <a href="#">
+                                                <i class="fa fa-shopping-cart"> Cart</i> <span class="badge badge-pill badge-danger">{{ count((array) session('cart')) }}</span>
+                                            </a>
+
+
+                                        </div>
+
+                                        <div class="account-dropdown__body">
                                             @php $total = 0 @endphp
                                             @foreach((array) session('cart') as $id => $details)
-                                                @php $total += $details['price'] * $details['quantity'] @endphp
+                                            @php $total += $details['price'] * $details['quantity'] @endphp
                                             @endforeach
-                                             
-                                                @if(session('cart'))
-                                                    @foreach(session('cart') as $id => $details)
-                                                        <div class="account-dropdown__item">
-                                                            <div class="image">
-                                                                <img src="{{ $details['image'] }}" />
-                                                            </div>
-                                                                <p>{{ $details['name'] }}</p>
-                                                                <span class="price text-info"> ${{ $details['price'] }}</span> <span class="count"> Quantity:{{ $details['quantity'] }}</span>
-                                                        </div>
-                                                    @endforeach
-                                                @endif
-                                                <div class="account-dropdown__item">
-                                                    <p>Total: <span class="text-info">$ {{ $total }}</span></p>
-                                                </div>
-                                            </div>
 
-                                            <div class="account-dropdown__footer">
-                                                    <a href="{{ route('cart') }}" class="btn btn-primary btn-block">View all</a>                                                                         
+                                            @if(session('cart'))
+                                            @foreach(session('cart') as $id => $details)
+                                            <div class="account-dropdown__item">
+                                                <div class="image">
+                                                    <img src="{{ $details['image'] }}" />
+                                                </div>
+                                                <p>{{ $details['name'] }}</p>
+                                                <span class="price text-info"> ${{ $details['price'] }}</span> <span class="count"> Quantity:{{ $details['quantity'] }}</span>
                                             </div>
+                                            @endforeach
+                                            @endif
+                                            <div class="account-dropdown__item">
+                                                <p>Total: <span class="text-info">$ {{ $total }}</span></p>
+                                            </div>
+                                        </div>
+
+                                        <div class="account-dropdown__footer">
+                                            <a href="{{ route('cart') }}" class="btn btn-primary btn-block">View all</a>
                                         </div>
                                     </div>
                                 </div>
-                            
+                            </div>
+
                             @if (Route::has('login'))
-                                @auth
-                            <div class="header-button">                                                     
+                            @auth
+                            <div class="header-button">
                                 <div class="account-wrap">
                                     <div class="account-item clearfix js-item-menu">
 
-                                   
-                                        
-                                            <div class="image">
-                                                <img src="{{ asset('theme/images/icon/avatar-01.jpg') }}" alt="John Doe" />
-                                            </div>
 
-                                            <div class="content">
-                                                <a class="js-acc-btn" href="#">{{ Auth::user()->name }}</a>
-                                            </div>
-                                            
-                                            <div class="account-dropdown js-dropdown">
-                                                <div class="info clearfix">
-                                                    <div class="image">
-                                                        <a href="#">
-                                                            <img src="{{ asset('theme/images/icon/avatar-01.jpg') }}" alt="John Doe" />
-                                                        </a>
-                                                    </div>
-                                                    <div class="content">
-                                                        <h5 class="name">
-                                                            <a href="#">{{ Auth::user()->name }}</a>
-                                                        </h5>
-                                                        <span class="email">{{ Auth::user()->email }}</span>
-                                                    </div>
-                                                </div>
-                                                <div class="account-dropdown__body">
-                                                    <div class="account-dropdown__item">
-                                                    @php
-                                                        $data = Auth::user()->id;
-                                                    @endphp
-                                                        <a href="{{ url("user/userDetails/{$data}") }}">
-                                                            <i class="zmdi zmdi-account"></i>View Account</a>
-                                                    </div>
-                                                    <div class="account-dropdown__item">
-                                                        <a href="#">
-                                                            <i class="zmdi zmdi-settings"></i>Setting</a>
-                                                    </div>
-                                                    <div class="account-dropdown__item">
-                                                        <a href="#">
-                                                            <i class="zmdi zmdi-money-box"></i>Billing</a>
-                                                    </div>
-                                                </div>
-                                                <div class="account-dropdown__footer">
-                                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                                        onclick="event.preventDefault();
-                                                                        document.getElementById('logout-form').submit();">
-                                                                        <i class="zmdi zmdi-power"></i>Logout</a>
+
+                                        <div class="image">
+                                            <img src="{{ asset('theme/images/icon/avatar-01.jpg') }}" alt="John Doe" />
+                                        </div>
+
+                                        <div class="content">
+                                            <a class="js-acc-btn" href="#">{{ Auth::user()->name }}</a>
+                                        </div>
+
+                                        <div class="account-dropdown js-dropdown">
+                                            <div class="info clearfix">
+                                                <div class="image">
+                                                    <a href="#">
+                                                        <img src="{{ asset('theme/images/icon/avatar-01.jpg') }}" alt="John Doe" />
                                                     </a>
-                                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                                        @csrf
-                                                    </form>
+                                                </div>
+                                                <div class="content">
+                                                    <h5 class="name">
+                                                        <a href="#">{{ Auth::user()->name }}</a>
+                                                    </h5>
+                                                    <span class="email">{{ Auth::user()->email }}</span>
                                                 </div>
                                             </div>
-                                                               
+                                            <div class="account-dropdown__body">
+                                                <div class="account-dropdown__item">
+                                                    @php
+                                                    $data = Auth::user()->id;
+                                                    @endphp
+                                                    <a href="{{ url("user/userDetails/{$data}") }}">
+                                                        <i class="zmdi zmdi-account"></i>View Account</a>
+                                                </div>
+                                                <div class="account-dropdown__item">
+                                                    <a href="#">
+                                                        <i class="zmdi zmdi-settings"></i>Setting</a>
+                                                </div>
+                                                <div class="account-dropdown__item">
+                                                    <a href="#">
+                                                        <i class="zmdi zmdi-money-box"></i>Billing</a>
+                                                </div>
+                                            </div>
+                                            <div class="account-dropdown__footer">
+                                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                                        document.getElementById('logout-form').submit();">
+                                                    <i class="zmdi zmdi-power"></i>Logout</a>
+                                                </a>
+                                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                                    @csrf
+                                                </form>
+                                            </div>
+                                        </div>
+
                                     </div>
-                                </div>          
-                            </div>   
+                                </div>
+                            </div>
                             @else
+                            <div>
+                                <a href="{{ route('login') }}">Log in</a>
+                                @if (Route::has('register'))
                                 <div>
-                                    <a href="{{ route('login') }}">Log in</a>                                  
-                                    @if (Route::has('register')) 
-                                    <div>
-                                        <a href="{{ route('register') }}" >Register</a>                     
-                                    </div>                                                                                            
-                                    @endif
-                                </div>                                
-                                @endauth                      
-                            @endif                            
+                                    <a href="{{ route('register') }}">Register</a>
+                                </div>
+                                @endif
+                            </div>
+                            @endauth
+                            @endif
                         </div>
                     </div>
                 </div>
@@ -193,31 +189,31 @@
             <!-- MAIN CONTENT-->
             <div class="main-content">
                 <div class="container">
-    
-                @if(session('success'))
+
+                    @if(session('success'))
                     <div class="alert alert-success">
                         {{ session('success') }}
-                    </div> 
-                @endif
+                    </div>
+                    @endif
 
-                @yield('content')
+                    @yield('content')
                 </div>
                 <div class="section__content section__content--p30">
                     <div class="container-fluid">
                         <div class="row">
                             @foreach($products as $product)
-                                <div class="col-xs-18 col-sm-6 col-md-3">
-                                    <div class="thumbnail">
-                                        <img src="{{ $product->P_imgPath }}" alt="">
-                                        <div class="caption">
-                                            <h4><a href=" {{ url("/user/productDetails/{$product -> P_id}") }}">{{ $product->P_name }}</a></h4>
-                                            <p><strong>Price: </strong> {{ $product->P_price }}$</p>
-                                            <p class="btn-holder"><a href="{{ route('add.to.cart', $product->P_id) }}" class="btn btn-warning btn-block text-center" role="button">Add to cart</a> </p>
-                                        </div>
+                            <div class="col-xs-18 col-sm-6 col-md-3">
+                                <div class="thumbnail">
+                                    <img src="{{ $product->P_imgPath }}" alt="">
+                                    <div class="caption">
+                                        <h4><a href=" {{ url("/user/productDetails/{$product -> P_id}") }}">{{ $product->P_name }}</a></h4>
+                                        <p><strong>Price: </strong> {{ $product->P_price }}$</p>
+                                        <p class="btn-holder"><a href="{{ route('add.to.cart', $product->P_id) }}" class="btn btn-warning btn-block text-center" role="button">Add to cart</a> </p>
                                     </div>
                                 </div>
+                            </div>
                             @endforeach
-                           
+
                         </div>
                     </div>
                 </div>
