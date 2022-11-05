d74dd71e564bce6c35c40de692f7cb114ba6cde1
jackduong93 <minhtrung9310@gmail.com>
2022-11-05 11:27:46 +0700

    trung feedback4

--- resources/views/feedback/create.blade.php
+++ resources/views/feedback/create.blade.php
@@ -2,24 +2,9 @@
 @section('content')
   
 <div class="card" style="margin:20px;">
-  <div class="card-header">Create New Feedback</div>
+  <div class="card-header">Thank you for your feedback</div>
   <div class="card-body">
-       
-      <form action="{{ url('/admin/product/feedback/create') }}" method="post">
-        {!! csrf_field() !!}
-        <label>Product Id</label></br>
-        <input type="text" name="P_id" id="P_id" class="form-control"></br>
-        <label>Name</label></br>
-        <input type="text" name="guestName" id="name" class="form-control"></br>
-        <label>Email</label></br>
-        <input type="text" name="guestEmail" id="email" class="form-control"></br>
-        <label>Subject</label></br>
-        <input type="text" name="subject" id="subject" class="form-control"></br>
-        <label>Comment</label></br>
-        <input type="text" name="comment" id="comment" class="form-control"></br>
-        <input type="submit" value="Save" class="btn btn-success"></br>
-    </form>
-    
+  <button><a href="{{ url("/") }}">Back to Home page</a></button>
   </div>
 </div>
   
