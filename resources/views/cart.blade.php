adc2c6c74152367140dedc77e6f871ed3044e3a0
jackduong93 <minhtrung9310@gmail.com>
2022-11-07 20:35:34 +0700

    trung cart variable bug

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -33,7 +33,7 @@
                 <tr data-id="{{ $id }}">
                     <td data-th="Product">
                         <div class="row">
-                            <div class="col-sm-3 hidden-xs"><img src="{{ asset('/image/'. $details['image'] }}" width="100" height="100" class="img-responsive"/></div>
+                            <div class="col-sm-3 hidden-xs"><img src="{{ $details['image'] }}" width="100" height="100" class="img-responsive"/></div>
                             <div class="col-sm-9">
                                 <h4 class="nomargin">{{ $details['name'] }}</h4>
                             </div>
