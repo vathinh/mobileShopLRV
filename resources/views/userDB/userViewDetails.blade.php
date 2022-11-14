6213af7c26e089f555fee7148a788ce48ab950c7
vathinh <tv.thinh2000@gmail.com>
2022-11-14 17:55:15 +0700

    update validate

--- resources/views/userDB/userViewDetails.blade.php
+++ resources/views/userDB/userViewDetails.blade.php
@@ -9,7 +9,7 @@
         <strong>Basic Form</strong> Elements
     </div>
     <div class="card-body card-block">
-        <form method="post" action="{{ url("/user/userDetailsUpdate/{$rs -> id}") }}" enctype="multipart/form-data" class="form-horizontal">
+        <!-- <form method="post" action="{{ url("/user/userDetailsUpdate/{$rs -> id}") }}" enctype="multipart/form-data" class="form-horizontal">
             @csrf
             <div class="row form-group">
                 <div class="col col-md-3">
@@ -52,7 +52,85 @@
                     
                     <input type="text" name="txtPhone" value="{{ $rs -> phone }}" class="form-control" >
                 </div>
+            </div> -->
+
+            <form method="post" action="{{ url("/user/userDetailsUpdate/{$rs -> id}") }}" enctype="multipart/form-data" class="form-horizontal">
+            @csrf
+
+            <div class="row mb-3">
+                <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+
+                <div class="col-md-6">
+                    <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ $rs -> name }}" required autocomplete="name" autofocus>
+
+                    @error('name')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
             </div>
+
+            <div class="row mb-3">
+                <label for="surname" class="col-md-4 col-form-label text-md-end">{{ __('Surname') }}</label>
+
+                <div class="col-md-6">
+                    <input id="surname" type="text" class="form-control @error('surname') is-invalid @enderror" name="surname" value="{{ $rs -> surname }}" required autocomplete="surname" autofocus>
+
+                    @error('surname')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="row mb-3">
+                <label for="address" class="col-md-4 col-form-label text-md-end">{{ __('Address') }}</label>
+
+                <div class="col-md-6">
+                    <input id="address" type="text" class="form-control @error('address') is-invalid @enderror" name="address" value="{{ $rs -> address }}" required autocomplete="address" autofocus>
+
+                    @error('address')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+
+            <div class="row mb-3">
+                <label for="phone" class="col-md-4 col-form-label text-md-end">{{ __('Phone') }}</label>
+
+                <div class="col-md-6">
+                    <input id="phone" type="text" class="form-control @error('phone') is-invalid @enderror" name="phone" value="{{ $rs -> phone }}" required autocomplete="phone" autofocus>
+
+                    @error('phone')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+
+
+            <div class="row mb-3">
+                <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+
+                <div class="col-md-6">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ $rs -> email }}" required autocomplete="email">
+
+                    @error('email')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+            </div>
+
+
 
             <div class="card-footer">
         <button type="submit" class="btn btn-primary btn-sm">
@@ -62,12 +140,12 @@
         <button class="btn btn-success btn-sm">
             <a href="{{ url("/user/change-password") }}">Change Password</a>
         </button>
-
-    </div>
         </form>
     </div>
-  
+        
 </div>
+  
+
 
 
 
