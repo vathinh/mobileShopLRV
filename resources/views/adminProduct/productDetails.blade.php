1af65588d05c9d29c5ec56216f55c23e57fc8deb
phuocthaoins <phuocthao.ins@gmail.com>
2022-11-16 17:42:11 +0700

    update readproduct

--- resources/views/adminProduct/productDetails.blade.php
+++ resources/views/adminProduct/productDetails.blade.php
@@ -10,14 +10,37 @@
             <div class="card">
               <!-- /.card-header -->
               <div class="card-body table-responsive p-0" $product as $product>
-                
-                            <h5>Name: <a href="<a href="{{ url("/admin/product/a_productDetails/{$product -> P_id}") }}">{{ $product -> P_name }}</a></h5> <br>
-                            <h5>ID: {{ $product -> P_id }} </h5> <br>
-                            <h5>Price: {{ $product -> P_price }}</h5> <br>
-                            <h5>Color: {{ $product -> P_color }}</h5> <br>
-                            <h5>Quantity: {{ $product -> P_quantity }}</h5> <br>
-                            <h5>Action: <a href="{{ url("/admin/product/updateProduct/{$product -> P_id}") }}">Update</a></h5> <br>
-                            <p>Image: <img src=" {{ asset('/image/'.$product-> P_imgPath) }} " alt="" style="width:260px;height: 260px;"></p>
+                <table> 
+                  <div style="text-align: center;"><h3>Product Info</h3></div>
+                  <tr>
+                    <td><h5>Name </h5></td>
+                    <td><h5><a href="<a href="{{ url("/admin/product/a_productDetails/{$product -> P_id}") }}">{{ $product -> P_name }}</a></h5></td>
+                  </tr>
+                  <tr>
+                    <td><h5>ID </h5></td>
+                    <td><h5>{{ $product -> P_id }}</h5></td>
+                  </tr>
+                  <tr>
+                    <td><h5>Price</h5></td>
+                    <td><h5>{{ $product -> P_price }}</h5></td>
+                  </tr>
+                  <tr>
+                    <td><h5>Color</h5></td>
+                    <td><h5>{{ $product -> P_color }}</h5></td>
+                  </tr>
+                  <tr>
+                    <td><h5>Quantity</h5></td>
+                    <td><h5>{{ $product -> P_quantity }}</h5></td>
+                  </tr>
+                  <tr>
+                    <td><h5>Action </h5></td>
+                    <td><h5><a href="{{ url("/admin/product/updateProduct/{$product -> P_id}") }}">Update</a></h5></td>
+                  </tr>
+                  <tr>
+                    <td><h5>Image</h5> </td>
+                    <td><p><img src=" {{ asset('/image/'.$product-> P_imgPath) }} " alt="" style="width:260px;height: 260px;"></p></td>
+                  </tr>
+                </table>                            
                 </div>
               <!-- /.card-body -->
             </div>
--- resources/views/adminProduct/readProduct.blade.php
+++ resources/views/adminProduct/readProduct.blade.php
@@ -10,16 +10,6 @@
       <div class="card">
         <div class="card-header">
           <h3 style="text-align: center">List of Products</h3>
-          <div class="card-tools">
-            <div class="input-group input-group-sm" style="width: 150px;">
-              <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-              <div class="input-group-append">
-                <button type="submit" class="btn btn-default">
-                  <i class="fas fa-search"></i>
-                </button>
-              </div>
-            </div>
-          </div>
         </div>
         <!-- /.card-header -->
         <div class="card-body table-responsive p-0">
@@ -28,7 +18,7 @@
 
               <th style="text-align: center;">ID</th>
               <th style="text-align: center;">Name</th>
-              <th style="text-align: center;">Price</th>
+              <th style="text-align: center;">Price ($)</th>
               <th style="text-align: center;">Storage</th>
               <th style="text-align: center;">Color</th>
               <th style="text-align: center;">Quantity</th>
