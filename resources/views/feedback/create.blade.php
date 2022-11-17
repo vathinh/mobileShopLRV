5ea0412b3203fff0dc51acdc6fee5e9e95ab0a8c
jackduong93 <minhtrung9310@gmail.com>
2022-11-17 13:10:19 +0700

    trung sua feedback

--- resources/views/feedback/create.blade.php
+++ resources/views/feedback/create.blade.php
@@ -1,12 +1,14 @@
 @extends('feedback.layout')
 @section('content')
-  
-<div class="card" style="margin:20px;">
-  {{ csrf_field() }}
-  <div class="card-header">Thank you for your feedback</div>
-  <div class="card-body">
-  <button><a href="{{ url("/") }}">Back to Home page</a></button>
+
+<div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content py-md-5 px-md-4 p-sm-3 p-4">
+        
+        <h3>Thank you for your feedback</h3>
+            <i class="fa fa-bell"></i>
+            <p class="r3 px-md-5 px-sm-1">We will check and reply to you soon</p>
+    
+            <div class="text-center mb-3"> <button class="btn btn-primary w-50 rounded-pill b1"><a style="color:white" href="{{ url("/") }}">Back to Home page</a></button> </div>
+        </div>
   </div>
-</div>
-  
 @stop
